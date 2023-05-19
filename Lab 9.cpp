3c5869a539562b16436f8e10118eb6b87018c5c3
Ali Abdallah <ali.abdallah43792@gmail.com>
2023-05-19 19:21:58 +0300

    Update

--- Lab 9.cpp
+++ Lab 9.cpp
@@ -14,31 +14,27 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 
-    int N, numOfProcessors;
-
+    int N;
     if (rank == 0)
     {
         printf("Please Enter the N value : \n");
         cin >> N;
-        printf("Please enter the number of processors : \n");
-        cin >> numOfProcessors;
     }
 
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&numOfProcessors, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int* arr = new int[N];
-    int local_arr[100];
+    int* local_arr = new int[N / size];
 
     if (rank == 0)
         for (int i = 0; i < N; i++)
             arr[i] = i;
 
 
-    MPI_Scatter(arr, 100, MPI_INT, &local_arr, 100, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(arr, N / size, MPI_INT, local_arr, N / size, MPI_INT, 0, MPI_COMM_WORLD);
 
     double local_sum = 0;
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < N / size; i++)
     {
         double x = (local_arr[i] - 0.5) / N;
         local_sum += 1 / (1 + x * x);
